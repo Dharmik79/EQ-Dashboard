9580696637a440973a9c2ca5f85805252a2b612e
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-03-18 00:58:03 -0600

    OuterDesign Implementation

---
+++ src/components/DepthChart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './DepthChart.css';
+function DepthChart() {
+  return (
+    <div className="depthview">
+    Depth Chart
+    </div>
+  )
+}
+
+export default DepthChart
