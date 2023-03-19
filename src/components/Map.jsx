b5793589a1ad4edc986c914edfef420902ae530c
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-19 13:48:44 -0500

    added the world map to the dashboard

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Map.css';
+import React, { useRef, useEffect } from "react";
+import "./Map.css";
+import data from "../custom.json";
+import { select, geoPath, geoMercator } from "d3";
 
 function Map() {
+  const svgRef = useRef();
+  const wrapperRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+   
+   const {width,height}=wrapperRef.current.getBoundingClientRect();
+ 
+   
+   // Projects the geo-coordinates on a 2D plane
+    const projection = geoMercator()
+    // Takes the gro json data and converts to attribute of d3 for a path of the svg element
+    const pathGenerator = geoPath().projection(projection);
+
+    svg
+      .selectAll(".country")
+      .data(data.features)
+      .join("path")
+      .attr("class", "country").attr("d",feature=>pathGenerator(feature));
+  });
+
   return (
-    <div className='map'> 
-      Maps
+    <div className="map">
+    <div ref={wrapperRef}>
+      <svg ref={svgRef}></svg>
     </div>
-  )
+    </div>
+  );
 }
 
-export default Map
+export default Map;
