a956a035d52007f2ee8bb85a198d087a18b26404
Dharmik79 <dholariyadharmik7@gmail.com>
2023-03-20 15:58:14 -0600

    added the line chart basics without the axes and the labels

--- src/components/DepthChart.jsx
+++ src/components/DepthChart.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-import './DepthChart.css';
+import React, { useState, useEffect, useRef } from "react";
+import "./DepthChart.css";
+import { select } from "d3-selection";
+import { line } from "d3-shape";
 function DepthChart() {
+  const [data, setData] = useState([25, 30, 45, 60, 20,65,75]);
+
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svg = select(svgRef.current);
+
+    const myLine = line()
+      .x((value, index) => index * 50)
+      .y((value) => value);
+
+    svg
+      .selectAll("path")
+      .data([data])
+      .join("path")
+      .attr("d", (value) => myLine(value)).
+      attr("fill","none").
+      attr("stroke","blue")
+  }, [data]);
   return (
     <div className="depthview">
-    Depth Chart
+      <svg ref={svgRef}></svg>
     </div>
-  )
+  );
 }
 
-export default DepthChart
+export default DepthChart;
