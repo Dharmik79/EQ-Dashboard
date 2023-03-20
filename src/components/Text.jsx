cbf275aa2ba2739faec83cdd303a5748bcb5f3ee
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-03-20 14:27:09 -0600

    Text view implemenetation

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import './Text.css';
+import React from "react";
+import "./Text.css";
 function Text() {
   return (
-    <div className='text'>
-      Text
+    <div className="text">
+      <div className="text-design">
+        <p className="h2">
+          WORLD'S <br></br>EARTHQUAKES{" "}
+        </p>
+        <span className="hot-zones">Data as 4 PM(PST), March 20, 2023</span>
+        
+        <div className="earthquakestats">
+          <hr/>
+          <div className="statsdata">
+            <div className="dataview">
+              <span>7.8</span>
+              <span>Max Magnitude</span>
+            </div>
+            <div className="dataview">
+              <span>1560310</span>
+              <span>Total Count</span>
+            </div>
+          </div>
+          <hr />
+        </div>
+
+        <div className="hot-zones">
+          <p>HOT ZONES:</p>
+          <ul>
+            <li>China </li>
+            <li>Turkey</li>
+            <li>Ukraine</li>
+            <li>Pakistan</li>
+            <li>Australia</li>
+          </ul>
+        </div>
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Text
+export default Text;
