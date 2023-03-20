61cf5f07467614d576349b33677e2c9f40718d30
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-19 21:46:15 -0500

    added the svg element for the world map to the center of the screen

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,28 +6,30 @@ import { select, geoPath, geoMercator } from "d3";
 function Map() {
   const svgRef = useRef();
   const wrapperRef = useRef();
+
   useEffect(() => {
     const svg = select(svgRef.current);
    
-   const {width,height}=wrapperRef.current.getBoundingClientRect();
- 
-   
-   // Projects the geo-coordinates on a 2D plane
-    const projection = geoMercator()
+   const {width,height}= wrapperRef.current.getBoundingClientRect();
+
+
+// Projects the geo-coordinates on a 2D plane
+const projection = geoMercator().fitSize([width,height],data)
+
     // Takes the gro json data and converts to attribute of d3 for a path of the svg element
     const pathGenerator = geoPath().projection(projection);
-
+   
     svg
       .selectAll(".country")
       .data(data.features)
       .join("path")
       .attr("class", "country").attr("d",feature=>pathGenerator(feature));
-  });
+  },[]);
 
   return (
     <div className="map">
     <div ref={wrapperRef}>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef} width="100%" height="100%"></svg>
     </div>
     </div>
   );
