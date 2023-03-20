55de94b16c9cd8a2e7af91bb976b2d7d1a206a80
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-19 22:00:09 -0500

    refactor the map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,28 +9,31 @@ function Map() {
 
   useEffect(() => {
     const svg = select(svgRef.current);
-   
-   const {width,height}= wrapperRef.current.getBoundingClientRect();
 
+    const { width, height } = wrapperRef.current.getBoundingClientRect();
 
-// Projects the geo-coordinates on a 2D plane
-const projection = geoMercator().fitSize([width,height],data)
+    // Projects the geo-coordinates on a 2D plane
+    const projection = geoMercator().fitSize([width, height], data);
 
     // Takes the gro json data and converts to attribute of d3 for a path of the svg element
     const pathGenerator = geoPath().projection(projection);
-   
+
     svg
       .selectAll(".country")
       .data(data.features)
       .join("path")
-      .attr("class", "country").attr("d",feature=>pathGenerator(feature));
-  },[]);
+      .attr("class", "country")
+      .attr("d", (feature) => pathGenerator(feature));
+  }, []);
 
   return (
     <div className="map">
-    <div ref={wrapperRef}>
-      <svg ref={svgRef} width="100%" height="100%"></svg>
-    </div>
+      <div
+        ref={wrapperRef}
+        style={{ display: "flex", justifyContent: "center", height: "100%" }}
+      >
+        <svg ref={svgRef} width="100%" height="100%"></svg>
+      </div>
     </div>
   );
 }
