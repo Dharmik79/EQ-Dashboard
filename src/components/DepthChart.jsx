dee26e254318ab7fd4d38549bdda83aa4baa27ea
Dharmik79 <dholariyadharmik7@gmail.com>
2023-03-20 16:18:55 -0600

    added the xaxis for the line chart

--- src/components/DepthChart.jsx
+++ src/components/DepthChart.jsx
@@ -6,32 +6,42 @@ import { axisBottom } from "d3-axis";
 import { scaleLinear } from "d3";
 function DepthChart() {
   const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
-  const xScale = scaleLinear()
-    .domain([0, data.length - 1])
-    .range([0, 300]);
 
-  const yScale=scaleLinear().domain([0,75]).range([150,0])
   const svgRef = useRef();
-  const xAxis = axisBottom();
 
   useEffect(() => {
     const svg = select(svgRef.current);
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300]);
+
+    const yScale = scaleLinear()
+      .domain([0, 75])
+      .range([150, 0]);
+  
+    const xAxis = axisBottom(xScale);
+
+    svg.select(".x-axis").style("transform","translateY(150px)").call(xAxis)
 
     const myLine = line()
-      .x((value, index) => xScale(index))
-      .y(yScale);
-
+    .x((value, index) => xScale(index))
+    .y(yScale);
     svg
-      .selectAll("path")
+      .selectAll(".line")
       .data([data])
       .join("path")
+      .attr("class","line")
       .attr("d", myLine)
       .attr("fill", "none")
       .attr("stroke", "blue");
+
+
   }, [data]);
   return (
     <div className="depthview">
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef} style={{ overflow: "visible"}}>
+        <g className="x-axis"></g>
+      </svg>
     </div>
   );
 }
