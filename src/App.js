978a344cd4e3fae1bae0d53d949d03bc5248957f
Dharmik79 <dholariyadharmik7@gmail.com>
2023-03-23 12:10:37 -0600

    added the earthquake points on the map

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import Text from './components/Text';
-import Map from './components/Map';
-import BarChart from './components/BarChart';
-import TimeLine from './components/TimeLine';
-import DepthChart from './components/DepthChart';
-
+import Text from "./components/Text";
+import Map from "./components/Map";
+import BarChart from "./components/BarChart";
+import TimeLine from "./components/TimeLine";
+import DepthChart from "./components/DepthChart";
+import commonApi from "./api/common";
 
 function App() {
+  const [data, setData] = useState(data);
+
+  useEffect(() => {
+    const getData = async () => {
+let result=await commonApi({ action: "getData" });
+console.log("resu",result)
+      setData(result)
+    };
+    getData();
+  }, []);
   return (
     <div className="dashboard">
-    <div className="row1">
-    <Text/>
-    <Map/>
-    </div>
-    <div className='row2'>
-    <BarChart/>
-    <DepthChart/>
-    </div>
-    <div className='row3'>
-    <TimeLine/>
-    </div>
+      <div className="row1">
+        <Text />
+        <Map data={data} />
+      </div>
+      <div className="row2">
+        <BarChart />
+        <DepthChart />
+      </div>
+      <div className="row3">
+        <TimeLine />
+      </div>
     </div>
   );
 }
