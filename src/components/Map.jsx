ecfc6cd50242daab611f3b5cf1ce4add173e2cba
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-25 16:12:10 -0500

    added the g element in svg to zoom in and out

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,35 +1,32 @@
 import React, { useRef, useEffect } from "react";
 import "./Map.css";
 import mapData from "../custom.json";
-import { select, geoPath, geoMercator } from "d3";
-import commonApi from "../api/common";
-import * as d3 from 'd3';
+import { select, geoPath, geoMercator, zoom as d3Zoom, event as d3Event } from "d3";
+
 function Map({ data }) {
   const svgRef = useRef();
   const wrapperRef = useRef();
- 
 
   useEffect(() => {
     const svg = select(svgRef.current);
-    
+    svg.selectAll("*").remove();
     const { width, height } = wrapperRef.current.getBoundingClientRect();
 
-    // Projects the geo-coordinates on a 2D plane
     const projection = geoMercator().fitSize([width, height], mapData);
-
-    // Takes the gro json data and converts to attribute of d3 for a path of the svg element
     const pathGenerator = geoPath().projection(projection);
 
-    
-  const zoom = d3.zoom().on("zoom", handleZoom);
+    const handleZoom = () => {
+      mapGroup.attr("transform", d3Event.transform);
+      circleGroup.attr("transform", d3Event.transform);
+    };
 
-  function handleZoom() {
-    
-      d3.select(svgRef.current).transition().attr("transform", d3.event.transform);
-    
-  }
+    const zoom = d3Zoom().on("zoom", handleZoom);
 
-    svg
+    // Create separate group elements for map and circles
+    const mapGroup = svg.append("g");
+    const circleGroup = svg.append("g");
+
+    mapGroup
       .selectAll(".country")
       .data(mapData.features)
       .enter()
@@ -37,27 +34,21 @@ function Map({ data }) {
       .attr("class", "country")
       .attr("d", (feature) => pathGenerator(feature))
       .attr("fill", "white")
-      .style("stroke", "black")
+      .style("stroke", "black");
 
     data &&
-      svg
+      circleGroup
         .selectAll(".circle")
         .data(data.features)
         .enter()
         .append("circle")
         .attr("class", "circle")
-        .attr("r", function(d) {
-          return Math.sqrt(d.properties.mag);
-        })
-        .attr("cx", function(d) {
-          return projection(d.geometry.coordinates)[0];
-        })
-        .attr("cy", function(d) {
-          return projection(d.geometry.coordinates)[1];
-        })
+        .attr("r", (d) => Math.sqrt(d.properties.mag))
+        .attr("cx", (d) => projection(d.geometry.coordinates)[0])
+        .attr("cy", (d) => projection(d.geometry.coordinates)[1])
         .attr("fill", "blue");
 
-        svg.call(zoom)
+    svg.call(zoom); // Apply the zoom behavior to the entire SVG
   }, [data]);
 
   return (
