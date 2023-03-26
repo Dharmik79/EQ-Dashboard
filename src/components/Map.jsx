f6277c071131e3f57114af03143c58c6b147be89
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-26 14:34:09 -0500

    added the funciton for the render the ison in the html

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,32 @@
-import React, { } from "react";
-import { Map as MapContainer, TileLayer} from "react-leaflet";
+import React, { useRef, useState } from "react";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+const eqIcon = new L.Icon({
+  iconUrl: "/public/earthquake.svg",
+  iconSize: [20, 20]
+});
+
+const icons = {};
+const fetchIcon = (count, size) => {
+  if (!icons[count]) {
+    icons[count] = L.divIcon({
+      html: `<div class="cluster-marker" style="width: ${size}px; height: ${size}px;">
+        ${count}
+      </div>`
+    });
+  }
+  return icons[count];
+};
 
 
 function Map({ data }) {
   return (
     <div className="map" style={{ height: "100%", width: "100%" }}>
       <MapContainer
-        center={[51.505, -0.09]}
-        zoom={1}
+        center={[52.6376, -1.135171]}
+        zoom={13}
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
