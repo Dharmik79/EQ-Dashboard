71c3fea5def4d05a71a088a45fe7fbf9f166c6b9
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-26 14:20:12 -0500

    added the leaflet map for the basci map instead of the svg

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,36 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Map as MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { } from "react";
+import { Map as MapContainer, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-// import Supercluster from "supercluster";
+
 
 function Map({ data }) {
-  const [clusters, setClusters] = useState([]);
-
-  // useEffect(() => {
-  //   if (!data) return;
-
-  //   const index = new Supercluster({
-  //     radius: 40,
-  //     maxZoom: 16,
-  //   });
-
-  //   index.load(
-  //     data.features.map((feature) => ({
-  //       type: "Feature",
-  //       properties: feature.properties,
-  //       geometry: { type: "Point", coordinates: feature.geometry.coordinates },
-  //     }))
-  //   );
-
-  //   const bounds = [
-  //     [-180, 85],
-  //     [180, -85],
-  //   ];
-
-  //   const newClusters = index.getClusters(bounds, 1);
-  //   setClusters(newClusters);
-  // }, [data]);
-
   return (
     <div className="map" style={{ height: "100%", width: "100%" }}>
       <MapContainer
@@ -42,37 +15,7 @@ function Map({ data }) {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {clusters.map((cluster) => {
-          const [longitude, latitude] = cluster.geometry.coordinates;
-          const {
-            cluster: isCluster,
-            point_count: pointCount,
-          } = cluster.properties;
-
-          if (isCluster) {
-            return (
-              <Marker
-                key={`cluster-${cluster.id}`}
-                position={[latitude, longitude]}
-              >
-                <Popup>
-                  <div>Cluster with {pointCount} points</div>
-                </Popup>
-              </Marker>
-            );
-          }
-
-          return (
-            <Marker
-              key={cluster.properties.id}
-              position={[latitude, longitude]}
-            >
-              <Popup>
-                <div>{cluster.properties.place}</div>
-              </Popup>
-            </Marker>
-          );
-        })}
+        
       </MapContainer>
     </div>
   );
