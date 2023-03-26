7314fc134487f158013186a7d51ad1424ccba503
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-25 21:18:42 -0500

    set the vg width as the width of the container

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -57,7 +57,7 @@ function Map({ data }) {
         ref={wrapperRef}
         style={{ display: "flex", justifyContent: "center", height: "100%" }}
       >
-        <svg ref={svgRef} width="1000" height="600"></svg>
+        <svg ref={svgRef} width="100%" height="600"></svg>
       </div>
     </div>
   );
