a56c93ed355feea6ddb2471e0986e67212dc10cf
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-03-27 14:42:05 -0600

    Added histogram for data

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,49 +1,90 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./BarChart.css";
 import { select } from "d3-selection";
-import { line } from "d3-shape";
 import { axisBottom, axisLeft } from "d3-axis";
 import { scaleLinear, scaleBand } from "d3";
 import * as d3 from "d3";
 
 function BarChart({ data }) {
   const svgRef = useRef();
-  let magArray = [];
-  const magData = {};
-  const margin = { top: 10, right: 30, bottom: 30, left: 40 };
-  const width = 500 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
 
-  if (data) {
-    data.features.map((feature) => {
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    const magData = {};
+
+    data.features.forEach((feature) => {
       const magType = Math.round(feature.properties.mag * 10) / 10;
-        if (magData[magType]) {
-          magData[magType].count += 1;
-        } else {
-          magData[magType] = {
-            count: 1,
-          };
-        }
+      if (magData[magType]) {
+        magData[magType].count += 1;
+      } else {
+        magData[magType] = {
+          count: 1,
+        };
+      }
     });
 
-    magArray = Object.keys(magData).map((mag) => ({
-      mag,
+    const sortedData = Object.keys(magData).map((mag) => ({
+      mag: parseFloat(mag),
       count: magData[mag].count,
     }));
+    const magArray = sortedData.sort((a, b) => a.mag - b.mag);
     console.log("magArray", magArray);
-  }
+    const uniqueMagnitudes = magArray.length;
+    const barWidth = 300 / uniqueMagnitudes - 1;
+    const x = scaleBand()
+      .domain(magArray.map((d) => d.mag))
+      .range([0, 300])
+      .paddingInner(0.1);
 
-  
+    const y = scaleLinear()
+      .domain([0, d3.max(magArray, (d) => d.count)])
+      .range([150, 0]);
+
+    const xAxis = axisBottom(x).tickFormat((d) => {
+      return Number.isInteger(d) ? d : "";
+    });
+    const yAxis = axisLeft(y);
+
+    select(svgRef.current)
+      .select(".x-axis")
+      .style("transform", "translateY(150px)")
+      .call(xAxis);
+
+    select(svgRef.current)
+      .select(".y-axis")
+      .style("transform", "translateX(0px)")
+      .call(yAxis);
+    const getColor = (mag) => {
+      if (mag >= 1 && mag < 2) return "steelblue";
+      if (mag >= 2 && mag < 3) return "pink";
+      if (mag >= 3 && mag < 4) return "yellow";
+      if (mag >= 4 && mag < 5) return "orange";
+      if (mag >= 5 && mag < 6) return "red";
+      return "gray";
+    };
+    const svg = select(svgRef.current);
+    svg
+      .selectAll(".bar")
+      .data(magArray)
+      .join("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.mag))
+      .attr("y", (d) => y(d.count))
+      .attr("width", barWidth)
+      .attr("height", (d) => 150 - y(d.count))
+      .attr("fill", (d) => getColor(d.mag))
+      .on("click", (d) => {
+        console.log("d", d);
+      });
+  }, [data]);
 
   return (
     <div className="barview">
       <p>Earthquake Magnitude Histogram</p>
-      <svg
-        ref={svgRef}
-        style={{ overflow: "visible" }}
-        width={width}
-        height={height + 50}
-      >
+      <svg ref={svgRef} style={{ overflow: "visible" }}>
         <g className="x-axis"></g>
         <g className="y-axis"></g>
       </svg>
