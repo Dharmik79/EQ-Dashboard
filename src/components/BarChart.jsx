91b6c8a67f5d0ad52356482c87e5e9f7f3172529
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-03-27 14:45:32 -0600

    Added color to bars

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -60,6 +60,9 @@ function BarChart({ data }) {
       .style("transform", "translateX(0px)")
       .call(yAxis);
     const getColor = (mag) => {
+      if (mag >= -2 && mag < -1) return "white";
+      if (mag >= -1 && mag < 0) return "blue";
+      if (mag >= 0 && mag < 1) return "black";
       if (mag >= 1 && mag < 2) return "steelblue";
       if (mag >= 2 && mag < 3) return "pink";
       if (mag >= 3 && mag < 4) return "yellow";
