fc3378af21f165de30e54c7b124c38146a4bf8d7
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-03-27 14:40:20 -0600

    Added magnitude & count for bar chart nearest val

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -8,61 +8,42 @@ import * as d3 from "d3";
 
 function BarChart({ data }) {
   const svgRef = useRef();
-  const magnitudeTypes = ["md", "ml", "ms_20", "mw", "me", "mi", "mb", "mb_lg"];
   let magArray = [];
-  const magTypeData = {};
-  const width = 700;
-  const height = 150;
+  const magData = {};
+  const margin = { top: 10, right: 30, bottom: 30, left: 40 };
+  const width = 500 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
 
   if (data) {
     data.features.map((feature) => {
-      const magType = feature.properties.magType;
-
-      if (magTypeData[magType]) {
-        magTypeData[magType].count += 1;
-      } else {
-        magTypeData[magType] = {
-          count: 1,
-        };
-      }
+      const magType = Math.round(feature.properties.mag * 10) / 10;
+        if (magData[magType]) {
+          magData[magType].count += 1;
+        } else {
+          magData[magType] = {
+            count: 1,
+          };
+        }
     });
-     magArray = Object.keys(magTypeData).map((magType) => ({
-      magType,
-      count: magTypeData[magType].count,
+
+    magArray = Object.keys(magData).map((mag) => ({
+      mag,
+      count: magData[mag].count,
     }));
-    console.log("magArray",magArray)
+    console.log("magArray", magArray);
   }
 
-  useEffect(() => {
-    const svg = select(svgRef.current);
-
-    const x = d3.scaleBand().domain(magnitudeTypes).range([0, width]).padding(0.1);
-
-    const y = d3.scaleLinear().domain([0, d3.max(magArray, (d) => d.count)]).range([height, 0]);
-
-    // Create the axes
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
-
-    svg.select(".x-axis").style("transform", `translateY(${height}px)`).call(xAxis);
-
-    svg.select(".y-axis").style("transform", "translateX(0px)").call(yAxis);
-
-    svg
-      .selectAll(".bar")
-      .data(magArray)
-      .join("rect")
-      .attr("class", "bar")
-      .attr("x", (d) => x(d.magType))
-      .attr("y", (d) => y(d.count))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => height - y(d.count));
-  }, [data]);
+  
 
   return (
     <div className="barview">
       <p>Earthquake Magnitude Histogram</p>
-      <svg ref={svgRef} style={{ overflow: "visible" }} width={width} height={height + 50}>
+      <svg
+        ref={svgRef}
+        style={{ overflow: "visible" }}
+        width={width}
+        height={height + 50}
+      >
         <g className="x-axis"></g>
         <g className="y-axis"></g>
       </svg>
