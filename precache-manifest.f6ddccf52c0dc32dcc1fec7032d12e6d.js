66ba087ac2e1992c9ab89e7a34b816ad46c12d34
Dharmik7 <dholariyadharmik7@gmail.com>
2023-03-27 22:52:40 -0500

    Updates

---
+++ precache-manifest.f6ddccf52c0dc32dcc1fec7032d12e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2f5441d56396c7e86a4",
+    "url": "/cs736-dashboard/static/css/main.28398be4.chunk.css"
+  },
+  {
+    "revision": "a2f5441d56396c7e86a4",
+    "url": "/cs736-dashboard/static/js/main.ed2c6ca0.chunk.js"
+  },
+  {
+    "revision": "a7e9c85973f3ece6d816",
+    "url": "/cs736-dashboard/static/js/runtime~main.e7d1871c.js"
+  },
+  {
+    "revision": "0fd8eafae27d7ac9b771",
+    "url": "/cs736-dashboard/static/css/2.280072e8.chunk.css"
+  },
+  {
+    "revision": "0fd8eafae27d7ac9b771",
+    "url": "/cs736-dashboard/static/js/2.72fe6d27.chunk.js"
+  },
+  {
+    "revision": "f7b805fe5161a7640c39ff018618e50a",
+    "url": "/cs736-dashboard/index.html"
+  }
+];
