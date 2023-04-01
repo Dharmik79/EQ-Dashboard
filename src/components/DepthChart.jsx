eff37c7c68199555122a40b1483611f037ebaeb6
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2023-04-01 12:06:55 -0600

    added the scatter plot for the depth and maginutde

--- src/components/DepthChart.jsx
+++ src/components/DepthChart.jsx
@@ -1,52 +1,67 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./DepthChart.css";
 import { select } from "d3-selection";
-import { line } from "d3-shape";
-import { axisBottom ,axisLeft} from "d3-axis";
-import { scaleLinear } from "d3";
-function DepthChart() {
-  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
 
+import { axisBottom, axisLeft } from "d3-axis";
+import { scaleLinear } from "d3";
+import { extent } from "d3-array";
+import getColor from "../config/color";
+function DepthChart({ data }) {
   const svgRef = useRef();
 
   useEffect(() => {
+    if (!data) return;
+
+    let magData = [];
+
+    data.features.forEach((feature) => {
+      const mag = Math.round(feature.properties.mag * 10) / 10;
+      const depth = feature.geometry.coordinates[2];
+      if (depth > 0 && mag > 0) {
+        magData.push({ depth: depth, magnitude: mag });
+      }
+    });
+
     const svg = select(svgRef.current);
     const xScale = scaleLinear()
-      .domain([0, data.length - 1])
-      .range([0, 300]);
+      .domain(extent(magData, (d) => d.depth))
+      .range([0, 400]);
 
     const yScale = scaleLinear()
-      .domain([0, 75])
+      .domain(extent(magData, (d) => d.magnitude))
       .range([150, 0]);
-  
-    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index=>index+1);
 
-    svg.select(".x-axis").style("transform","translateY(150px)").call(xAxis)
+    const xAxis = axisBottom(xScale);
+
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
 
     const yAxis = axisLeft(yScale);
 
-    svg.select(".y-axis").style("transform","translateX(0px)").call(yAxis)
-    const myLine = line()
-    .x((value, index) => xScale(index))
-    .y(yScale);
+    svg.select(".y-axis").style("transform", "translateX(0px)").call(yAxis);
+
     svg
-      .selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("class","line")
-      .attr("d", myLine)
-      .attr("fill", "none")
-      .attr("stroke", "blue");
-
-
+      .selectAll(".circle")
+      .data(magData)
+      .join("circle")
+      .attr("class", "circle")
+      .attr("cx", (d) => xScale(d.depth))
+      .attr("cy", (d) => yScale(d.magnitude))
+      .attr("r", 1)
+      .attr("fill", (d) => getColor(d.magnitude));
   }, [data]);
   return (
     <div className="depthview">
-       <div style={{ display: "flex", justifyContent: "space-between" ,alignItems:"center"}}>
-    <p className="bar-chart-name">Depth Chart Analysis</p>
-    <button className="button-style"> Reset Brush</button>
-  </div>
-      <svg ref={svgRef} style={{ overflow: "visible"}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <p className="bar-chart-name">Depth Chart Analysis</p>
+        <button className="button-style"> Reset Brush</button>
+      </div>
+      <svg ref={svgRef} style={{ overflow: "visible" }}>
         <g className="x-axis"></g>
         <g className="y-axis"></g>
       </svg>
