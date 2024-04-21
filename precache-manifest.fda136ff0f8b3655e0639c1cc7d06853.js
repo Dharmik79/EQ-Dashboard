e0cb10569b3f677f50f0abaf518c2e05015cdd3c
Dharmik79 <ddholariya199@gmail.com>
2024-04-21 16:45:09 -0600

    Updates

---
+++ precache-manifest.fda136ff0f8b3655e0639c1cc7d06853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa78cfc832e62e08c124",
+    "url": "/eq-dashboard/static/css/main.2bee6ade.chunk.css"
+  },
+  {
+    "revision": "aa78cfc832e62e08c124",
+    "url": "/eq-dashboard/static/js/main.0aec380b.chunk.js"
+  },
+  {
+    "revision": "127852a5c643ab9271a4",
+    "url": "/eq-dashboard/static/js/runtime~main.c02b92d8.js"
+  },
+  {
+    "revision": "444e620beca2f1801cfc",
+    "url": "/eq-dashboard/static/css/2.d09a3771.chunk.css"
+  },
+  {
+    "revision": "444e620beca2f1801cfc",
+    "url": "/eq-dashboard/static/js/2.13ada42c.chunk.js"
+  },
+  {
+    "revision": "7b1c62e8e485abe3ea3e62412a858ab9",
+    "url": "/eq-dashboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.769ffc90eaae37a7cd4cf6d437d96b2a.js"
+  "/eq-dashboard/precache-manifest.fda136ff0f8b3655e0639c1cc7d06853.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/eq-dashboard/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
